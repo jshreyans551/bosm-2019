26de3b0361634eabd37ff5d385573640b1d19bf1
chirag-singhal <csinghal208@gmail.com>
2019-04-24 12:04:40 +0530

    scroll

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,7 +2,6 @@ var wraper = document.getElementsByClassName('wrapper')[0];
 var minDistance = 30;
 var noNavigation = 4;
 var navigators = document.querySelectorAll(".navigator .navigation");
-var input = document.getElementsByTagName("input");
 var currentNavigator = 0;
 var current_page = 1;
 var step = 0, x = 0;
@@ -10,12 +9,7 @@ var last_page = 4;
 var play = false;
 var sY, dY;
 
-input.onfocus = function () {
-    setTimeout(function() {
-        window.scrollTo(0, 0);
-        document.body.scrollTop = 0;
-    }, 50)
-}
+wraper.addEventListener('wheel', scrollY);
 
 function scrollY(e){
     e.preventDefault();
