f008c73959d922c67374ed6eb906b129e652d132
mukul0596 <icecoolmkl@gmail.com>
2019-04-25 15:44:21 +0530

    virtual keyboard layout bug fixes

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -121,7 +121,7 @@ function shiftUp() {
     if(window.innerWidth < 400)
     document.querySelector(".register").style.transform = "translateY(-11vw)";
     document.querySelector(".nav-bar").style.transform = "translateY(-100%)";
-    setTimeout(function() {alert(window.innerHeight);}, 1000);
+    setTimeout(function() {alert(window.innerHeight);}, 5000);
 }
 
 function shiftDown() {
