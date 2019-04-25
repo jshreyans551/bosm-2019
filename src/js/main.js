abf8fbf0b967bdedb0e6cda89b98d1d8002d12ab
meliodas113 <f20180433@pilani.bits-pilani.ac.in>
2019-04-25 16:05:33 +0530

    changes hamburger style

--- src/js/main.js
+++ src/js/main.js
@@ -119,16 +119,16 @@ function navbar()
 if(count == 0)
 {
     document.getElementsByClassName("hamburger")[0].style.transform = "translateX(0vw)";
-    document.getElementsByClassName("ham")[0].style.transform = "rotate(45deg)";
-    document.getElementsByClassName("ham")[1].style.opacity = "0";
-    document.getElementsByClassName("ham")[2].style.transform = "rotate(-45deg)";
+    document.getElementsByClassName("ham")[0].style.transform = "rotate(135deg)";
+    document.getElementsByClassName("ham")[1].style.transform = "translateX(-60vw)";
+    document.getElementsByClassName("ham")[2].style.transform = "rotate(-135deg)";
     count++;
 }
 else if (count == 1)
 {
     document.getElementsByClassName("hamburger")[0].style.transform = "translateX(-60vw)";
     document.getElementsByClassName("ham")[0].style.transform = "translateY(-1.2vh) rotate(0deg)";
-    document.getElementsByClassName("ham")[1].style.opacity = "1";
+    document.getElementsByClassName("ham")[1].style.transform = "translateX(0vw)";
     document.getElementsByClassName("ham")[2].style.transform = "translateY(1.2vh) rotate(0deg)";
     count--;
 }
