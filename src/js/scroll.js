102134997fefc618073ab36246d9f1298bf3524a
mukul0596 <icecoolmkl@gmail.com>
2019-04-25 16:53:17 +0530

    virtual keyboard layout bug fixes

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -106,14 +106,14 @@ if(window.innerWidth < 500) {
     document.getElementById("first-name").addEventListener("focus", shiftUp);
     document.getElementById("last-name").addEventListener("focus", shiftUp);
     document.getElementById("email").addEventListener("focus", shiftUp);
-    document.getElementById("contact-no").addEventListener("focus", shiftUp);
-    document.getElementById("college").addEventListener("focus", shiftUp);
+    document.getElementById("contact-number").addEventListener("focus", shiftUp);
+    document.getElementById("college-name").addEventListener("focus", shiftUp);
     
     document.getElementById("first-name").addEventListener("focusout", shiftDown);
     document.getElementById("last-name").addEventListener("focusout", shiftDown);
     document.getElementById("email").addEventListener("focusout", shiftDown);
-    document.getElementById("contact-no").addEventListener("focusout", shiftDown);
-    document.getElementById("college").addEventListener("focusout", shiftDown);
+    document.getElementById("contact-number").addEventListener("focusout", shiftDown);
+    document.getElementById("college-name").addEventListener("focusout", shiftDown);
 
     window.addEventListener("resize", function() {
         if(window.innerHeight > (screen.height-200)){
