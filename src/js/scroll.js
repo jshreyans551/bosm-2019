4bf3a9389eb0a06ae141be31843746d989b998bd
mukul0596 <icecoolmkl@gmail.com>
2019-05-21 23:04:54 +0530

    bug fixes

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -122,8 +122,8 @@ function changeNavigator(dir) {
 
 
 if(window.innerWidth < 500) {
-    android:windowSoftInputMode="adjustPan";
-    document.write('<meta name="viewport" content="width=device-width, height='+window.innerHeight+', initial-scale=1.0">');
+    // android:windowSoftInputMode="adjustPan";
+    // document.write('<meta name="viewport" content="width=device-width, height='+window.innerHeight+', initial-scale=1.0">');
 
     document.getElementById("first-name").addEventListener("focus", shiftUp);
     document.getElementById("last-name").addEventListener("focus", shiftUp);
